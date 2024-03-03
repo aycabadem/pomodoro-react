440cd51a82134965837648b1b1d92b84d526f2f6
Ayca Badem <aycabadem12@gmail.com>
2024-03-03 18:19:43 +0000

    add redux

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,19 +1,41 @@
 import React from "react";
 import ReactSlider from "react-slider";
 import "./Slider.css";
+import { useDispatch, useSelector } from "react-redux";
+import { setWorkMinutes, setBreakMinutes } from "./redux/settingsSlice";
+import { RootState } from "./redux/store";
+
 const Settings = () => {
+  const dispatch = useDispatch();
+  const workMinutes = useSelector(
+    (state: RootState) => state.settings.workMinutes
+  );
+  const breakMinutes = useSelector(
+    (state: RootState) => state.settings.breakMinutes
+  );
   return (
     <div style={{ textAlign: "center" }}>
-      <label>Work min:</label>
+      <label>Work min: {workMinutes} </label>
       <ReactSlider
         className={"slider"}
         thumbClassName={"thumb"}
         trackClassName={"track"}
-        value={45}
+        value={workMinutes}
         min={1}
-        max={120}
+        max={100}
+        onChange={(value) => dispatch(setWorkMinutes(value as number))}
       ></ReactSlider>
-      <label>Break min:</label>
+
+      <label>Break min: {breakMinutes} </label>
+      <ReactSlider
+        className={"slider"}
+        thumbClassName={"thumb"}
+        trackClassName={"track"}
+        value={breakMinutes}
+        min={1}
+        max={100}
+        onChange={(value) => dispatch(setBreakMinutes(value as number))}
+      ></ReactSlider>
     </div>
   );
 };
